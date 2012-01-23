a4a2b11501a2d734574ec1a868ad4e23654c136b
Karel B <running@bilek.vs.fortion.net>
2012-01-23 17:47:05 +0100

    .......

--- skola_ML/data/PFL/shared.R
+++ skola_ML/data/PFL/shared.R
@@ -1,59 +1,31 @@
 library(rpart)
+library(e1071)
 
 all_table<-read.table("current_results/all_data")
 
-default_opts <- function(type) {
-        wat = c(min_split=20, 
-                splitting_type = "gini",
-                should_prune=0,
-                c_p=0.01);
-        return (wat);
-}
-get_opts_grid <- function() {
-    return (expand.grid( 
-        should_prune = c(0,0.001, 0.002,0.005,0.01,0.02,0.05, 0.1, 0.2, 0.5) , 
-        splitting_type = c("gini", "information"),
-        min_split = c(1, 2, 5, 10, 20, 50, 100),
-        c_p = c(0.001, 0.002,0.005,0.01,0.02,0.05,0.1,0.2,0.5)
-    ));
-}
 
-classify <- function(formula, train_data, test_data, opts) {
-
-    classifier<-rpart(
-        formula, 
-        data = train_data, 
-        method = "class",
-        parms = list (
-            split = opts["splitting_type"]
-        ), 
-        control = rpart.control(
-            cp = as.numeric(opts["c_p"]),
-            minsplit = as.numeric(opts["min_split"])
-        )
-    )
-    
-    if (as.numeric(opts["should_prune"]) > 0) {
-        classifier<-prune(
-            classifier , 
-            cp = as.numeric(opts["should_prune"])
-        )
-    }
-
-    found_classes <- predict(classifier, test_data, type = "class")
-    
-    return(found_classes);
-}
-
-try <- function(train_range, test_range, features, opts) {
+try <- function(train_range, test_range, features, type, tune, boost) {
         test_table_without_class <- all_table[test_range, -1]
         correct_classes <- all_table[test_range, 1]
         train_table <- all_table[train_range,]
         names <- names(test_table_without_class)[features==1]
         
         formula <- as.formula(paste("semantic_class ~ ", paste(names, collapse= "+")))
-       
-        found_classes <- classify(formula, train_table, test_table_without_class, opts)
+      
+        if (type=="DT") {
+            if (tune==0) {
+               classifier<-rpart(
+                    formula, 
+                    data = train_table, 
+                    method = "class")
+            } else {
+                classifier<-best.rpart(
+                    formula,
+                    data = train_table,
+                    ) 
+            }
+            found_classes <- predict(classifier, test_table_without_class, type = "class")
+        }
         
         same <- found_classes == correct_classes
         correctness<- length(same[same])
--- skola_ML/data/PFL/skript_logika.R
+++ skola_ML/data/PFL/skript_logika.R
@@ -1,29 +1,4 @@
-library(rpart)
-
-all_table<-read.table("current_results/all_data")
-
-
-try <- function(train_range, test_range, features, opts) {
-        test_table_without_class <- all_table[test_range, -1]
-        correct_classes <- all_table[test_range, 1]
-        train_table <- all_table[train_range,]
-        names <- names(test_table_without_class)[features==1]
-        
-        formula <- as.formula(paste("semantic_class ~ ", paste(names, collapse= "+")))
-       
-        tuneobj <- tune.rpart(formula, train_table);
-        classifier <- best.rpart(formula, tuneobj)
-
-        found_classes <- predict(classifier, test_table_without_class, 
-                    type = "class")
-
-
-        
-        same <- found_classes == correct_classes
-        correctness<- length(same[same])
-        return(correctness/length(test_range))
-}
-
+source("shared.R");
 working_range<- 1:220
 test_range<- 221:250
 
@@ -32,8 +7,8 @@ features_to_take <- scan("current_results/feature_took_final")
 result <- try(
             working_range, test_range,
                 features_to_take,
-                opts
-            );
+           "DT", 1,0
+           );
  
 
-write(result, "test_result")
+print(result);
