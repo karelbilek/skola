46e94e1e1b5173fccf5e3bbff7081c12dc5b0f3e
Karel B <running@bilek.vs.fortion.net>
2012-01-24 04:51:23 +0100

    AAAAAA

--- skola_ML/data/PFL/transform.pl
+++ skola_ML/data/PFL/transform.pl
@@ -494,7 +494,7 @@ if (1 or ($ARGV[1]//"") eq "binary") {
     }
 }
 
-my $treshold = 7;
+my $treshold = 3;
 #najit ty, co se nebudou brat, protoze jsou vsude stejne
 COLUMN:
 for my $column (2..$#head) {
