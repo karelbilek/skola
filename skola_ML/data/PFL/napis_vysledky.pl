fe10e811f5195d312f78d09bebcffd8e8872db9f
Karel B <running@bilek.vs.fortion.net>
2012-03-15 18:47:27 +0100

    LOOOOL

--- skola_ML/data/PFL/napis_vysledky.pl
+++ skola_ML/data/PFL/napis_vysledky.pl
@@ -9,6 +9,7 @@ for my $result_filename (<results/*.result.*>) {
     my $res = read_file("$result_filename");
     chomp $res;
     my ($size, $error) = $res =~ /^(\S+)\s+(\S+)$/;
+    if (length($way) < 6) {$way.=" "x(6-length($way))}
     print $way."\t".$word."\t".$size."\t".$error."\t".($size +
     $error)."\t".($size - $error)."\n" if ($word eq $wantword);
 }
