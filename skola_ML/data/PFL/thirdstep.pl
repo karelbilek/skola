c42dfb08cad98a203d346227955348be5cd5a859
Karel B <running@bilek.vs.fortion.net>
2012-03-18 05:45:18 +0100

    LOOOOL

--- skola_ML/data/PFL/thirdstep.pl
+++ skola_ML/data/PFL/thirdstep.pl
@@ -15,7 +15,29 @@ sub run_sys {
 my %settings_for_word = (ally=>{type=>'2', model=> 'SVM', options => 
     '"scale" "kernel" "degree" "gamma" "coef0" "cost" "shrinking" "probability"
     "1379" TRUE "polynomial" 2 0.1 1 1 TRUE TRUE'},
-    );
+    
+    arrive=>{model=>'boosting', type=>'2', options=>
+    '"min_split" "c_p" "mfinal" "boos" "coeflearn"
+    "5" 10 0.01 100 TRUE "Breiman"'},
+    
+     cry=>{model=>'SVM', type=>'2', options=>
+    '"scale" "kernel" "degree" "gamma" "coef0" "cost" "shrinking" "probability"
+    "692" FALSE "polynomial" 3 0.2 0 0.5 TRUE TRUE'},
+
+     halt=>{model=>'bagging', type=>'2', options=>
+     '"min_split" "c_p" "mfinal"
+     "27" 100 0.2 200'},
+
+    plough=>{model=>'SVM', type=>2, options=>
+    '"scale" "kernel" "degree" "gamma" "coef0" "cost" "shrinking" "probability"
+    "324" FALSE "polynomial" 2 0.2 5 0.1 TRUE TRUE'},
+
+    submit=>{model=>'bagging', type=>2, options=>
+    '"min_split" "c_p" "mfinal"
+    "8" 50 0.2 10'},
+
+
+    });
 my %settings = %{$settings_for_word{$word}};
 
 use File::Slurp;
