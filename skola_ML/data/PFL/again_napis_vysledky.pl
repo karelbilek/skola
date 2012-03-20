35846a32e87b2e306bab8173a4692d2a223098cc
Karel B <running@bilek.vs.fortion.net>
2012-03-20 03:05:22 +0100

    LOOOOL

---
+++ skola_ML/data/PFL/again_napis_vysledky.pl
@@ -0,0 +1,15 @@
+use warnings;
+use strict;
+
+my $wantword = $ARGV[0];
+
+for my $result_filename (<results/*.result2.*>) {
+    my ($way, $word) = $result_filename =~ m{results/(.*)\.result\.(.*)};
+    use File::Slurp;
+    my $res = read_file("$result_filename");
+    chomp $res;
+    my ($size, $error) = $res =~ /^(\S+)\s+(\S+)$/;
+    if (length($way) < 9) {$way.="."x(9-length($way))}
+    print $way."\t".$word."\t".$size."\t".$error."\t".($size +
+    $error)."\t".($size - $error)."\n" if ($word eq $wantword);
+}
